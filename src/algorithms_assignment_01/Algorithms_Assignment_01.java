17639e935adf926cadcfcf202983c66ede365d51
Hallur20 <hvn15@hotmail.com>
2019-02-12 10:50:18 +0100

    we can read file

---
+++ src/algorithms_assignment_01/Algorithms_Assignment_01.java
@@ -0,0 +1,12 @@
+package algorithms_assignment_01;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Algorithms_Assignment_01 {
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        Logic l = new Logic();
+       l.readFile();
+    }
+}
